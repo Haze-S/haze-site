8f66e4a947110e3447d1b3ee90d73c7e65fa04b4
haze <skdml20022@gmail.com>
2024-03-24 13:59:30 +0900

    feat: root page 생성

---
+++ src/components/Main/PostList.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent } from "react";
+import styled from "@emotion/styled";
+import PostItem from "./PostItem";
+
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2020.01.29.',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    'https://media.gcflearnfree.org/content/5e31ca08bc7eff08e4063776_01_29_2020/ProgrammingIllustration.png',
+  link: 'https://www.google.co.kr/',
+}
+
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+`
+
+const PostList: FunctionComponent = function () {
+  return <PostListWrapper>
+    <PostItem {...POST_ITEM_DATA} />
+    <PostItem {...POST_ITEM_DATA} />
+    <PostItem {...POST_ITEM_DATA} />
+    <PostItem {...POST_ITEM_DATA} />
+  </PostListWrapper>
+}
+
+export default PostList
